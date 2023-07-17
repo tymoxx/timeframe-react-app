769b26a0124f22417a8f36cd7e2337e2d2a44ef6
tymoxx <7777kara@gmail.com>
2023-07-17 14:48:16 +0200

    adds a second image

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {gsap} from 'gsap';
 import {ScrollMagicPluginGsap} from "scrollmagic-plugin-gsap";
@@ -7,36 +7,47 @@ import ScrollMagic from 'scrollmagic';
 import phoneMockup from './assets/phone-mockup.png';
 import screen1 from './assets/screen1.jpg';
 import image1 from './assets/image1.jpg';
+import image2 from './assets/image2.jpg';
+
 ScrollMagicPluginGsap(ScrollMagic, gsap); // add gsap to ScrollMagic
 ScrollMagicPluginIndicator(ScrollMagic); // add indicators plugin
 
 const DURATION = 1000;
 function App() {
-    const imgRef = useRef();
-
     useEffect(() => {
         const controller = new ScrollMagic.Controller();
 
-        // Calculate half of the width difference between the image and the viewport
-        const xOffset = (imgRef.current.offsetWidth - window.innerWidth) / 2;
-
-        // Move horizontally
+        // MOVE HORIZONTALLY
+        /** Img 1 */
         new ScrollMagic.Scene({
             triggerElement: ".animation", duration: DURATION,
             triggerHook: 0, // trigger at the top of viewport
         })
             // .addIndicators()
             .setTween(gsap.timeline()
-                .add(gsap.from('#pin1', 1, {x: '38vw', y: '5vw'}))
-                .add(gsap.to('#pin1', 0.5, {rotate: 30}), '<')
-                .add(gsap.to('#pin1', 0.5, {rotate: 0}), '>')
+                .add(gsap.from('#id1', 1, {x: '27vw', y: '10vw'}))
+                .add(gsap.to('#id1', 0.3, {rotate: 20}), '<')
+                .add(gsap.to('#id1', 0.7, {rotate: 0}), '>')
             )
-            .setPin("#pin1") // pin the image when it's in the center of the viewport
+            .setPin("#id1") // pin the image when it's in the center of the viewport
             .addTo(controller)
             .addIndicators()
 
+        /** Img 1 */
+        new ScrollMagic.Scene({
+            triggerElement: ".animation", duration: DURATION,
+            triggerHook: 0,
+        })
+            .setTween(gsap.timeline()
+                .add(gsap.from('#id2', 1, {x: '38vw', y: '5vw'}))
+                .add(gsap.to('#id2', 0.5, {rotate: -30}), '<')
+                .add(gsap.to('#id2', 0.5, {rotate: 0}), '>')
+            )
+            .setPin("#id2")
+            .addTo(controller)
 
-        /* Fix vertically */
+
+        /* FIX VERTICALLY */
         new ScrollMagic.Scene({
             triggerElement: ".animation", duration: DURATION,
             triggerHook: 0, // trigger at the top of viewport
@@ -59,10 +70,14 @@ function App() {
 
         <section className={"animation"}>
             <img
-                ref={imgRef}
-                id="pin1"
+                id="id1"
                 src={image1}
-                className={'image'}
+                className={['image', 'image1'].join(' ')}
+            />
+            <img
+                id="id2"
+                src={image2}
+                className={['image', 'image2'].join(' ')}
             />
 
             <img
