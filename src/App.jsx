672dc3220e528641fd39bb8f1cd856d449c008ec
tymoxx <7777kara@gmail.com>
2023-07-17 12:09:43 +0200

    adds timeline to make animation in stages.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ScrollMagic from 'scrollmagic';
 ScrollMagicPluginGsap(ScrollMagic, gsap); // add gsap to ScrollMagic
 ScrollMagicPluginIndicator(ScrollMagic); // add indicators plugin
 
+const DURATION = 2000;
 function App() {
     const imgRef = useRef();
 
@@ -19,18 +20,20 @@ function App() {
 
         //* Move horizontally */
         new ScrollMagic.Scene({
-            triggerElement: ".Animation",
-            duration: 400,
+            triggerElement: ".Animation", duration: DURATION,
         })
             // .addIndicators()
-            .setTween(gsap.from('#pin1', 1, {x: -xOffset}))
+            .setTween(gsap.timeline()
+                .add(gsap.from('#pin1', 1, {x: '40vw', y: '5vw'}))
+                .add(gsap.to('#pin1', 0.5, {rotate: 30}), '<')
+                .add(gsap.to('#pin1', 0.5, {rotate: 0}), '>')
+            )
             .setPin("#pin1") // pin the image when it's in the center of the viewport
             .addTo(controller);
 
         /* Fix vertically */
         new ScrollMagic.Scene({
-            triggerElement: ".Animation",
-            duration: 400,
+            triggerElement: ".Animation", duration: DURATION,
         })
             .addIndicators()
             .setPin("#phone-image")
@@ -38,31 +41,29 @@ function App() {
 
     }, []);
 
-    return (
-        <div className="App">
-            <header className="header">
-                HEADER
-            </header>
+    return (<div className="App">
+        <header className="header">
+            HEADER
+        </header>
 
-            <section className={"Animation"}>
-                <img
-                    ref={imgRef}
-                    id="pin1"
-                    src={'https://assets.website-files.com/5c6648378238e311a00c7e61/5c6648378238e318c30c7f6d_hero-002.jpg'}
-                    className={'image'}
-                />
+        <section className={"Animation"}>
+            <img
+                ref={imgRef}
+                id="pin1"
+                src={'https://assets.website-files.com/5c6648378238e311a00c7e61/5c6648378238e318c30c7f6d_hero-002.jpg'}
+                className={'image'}
+            />
 
-                <img
-                    id="phone-image"
-                    src={'https://assets.website-files.com/5c6648378238e311a00c7e61/5c6648378238e318c30c7f6d_hero-002.jpg'}
-                    className={'phone-image'}
-                />
-            </section>
-            <footer className="footer">
-                FOOTER
-            </footer>
-        </div>
-    );
+            <img
+                id="phone-image"
+                src={'https://assets.website-files.com/5c6648378238e311a00c7e61/5c6648378238e318c30c7f6d_hero-002.jpg'}
+                className={'phone-image'}
+            />
+        </section>
+        <footer className="footer">
+            FOOTER
+        </footer>
+    </div>);
 }
 
 export default App;
